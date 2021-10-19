7352c1ce45b1cdecfa8edaa3227e0bfeaa9e2baa
RozenAstrayChen <rozendhyan@gmail.com>
2021-10-19 11:04:19 +0800

    finish mapReducer

--- mapper.py
+++ mapper.py
@@ -1,13 +1,32 @@
 import sys
+import os
+import pandas as pd
+import csv
+import re
+from signal import signal, SIGPIPE, SIG_DFL, SIG_IGN
+
+def skip_gram(tokens, f_max_dis = 5, p_max_dis = -5):
+    token_length = len(tokens)
+    
+    gram = []
+
+    for idx in range(token_length):
+        # pervious
+        for i in range(-1, p_max_dis-1, -1):
+            if idx+i >= 0:
+                gram.append((tokens[idx], tokens[idx+i], i, 1))
+        for i in range(1, f_max_dis+1):
+            if idx+i < token_length:
+                gram.append((tokens[idx], tokens[idx+i], i, 1))
+
+    return gram
 
 def preprocess(text):
     # preprocess/tokenize the sentence
-    processed_data = ...
+    processed_data = re.sub(r'[^\w\s]',' ',text).lower().split()
     return processed_data
 
 def _map(text: str):
-    tokens = preprocess(text)
-
     # [ TODO ] generate the mapper output
     # Output: "{pivot}\t{word}\t{distance}\t{count}"
     # Example: 
@@ -17,8 +36,16 @@ def _map(text: str):
     #   predict the 1   1
     #   ...
     ...
+    tokens = preprocess(text)
+    mapper = skip_gram(tokens)
+    return mapper
     
 if __name__== "__main__" :
+    signal(SIGPIPE, SIG_IGN)
     for line in sys.stdin:
-        _map(line)
+        mappers = _map(line)
+        #rint(mappers)
+        for mapper in mappers:
+            print('%s\t%s\t%s\t%s' % (mapper[0], mapper[1], mapper[2], mapper[3]))
+        #break # read all file
      
--- reducer.py
+++ reducer.py
@@ -1,11 +1,36 @@
 import sys
+import os
+import pandas as pd
+import csv
+import re
+import ast
+from signal import signal, SIGPIPE, SIG_DFL, SIG_IGN
+
+def generate_reducer(reducer_list, skip_list, index=0, overwrite=False):
+    if overwrite == False:
+        # generate new
+        init_list = [skip_list[0], skip_list[1], int(skip_list[3]), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # 3..12
+        
+        if int(skip_list[2])>0:
+            init_list[7+int(skip_list[2])] += int(skip_list[3])
+        else:
+            init_list[8+int(skip_list[2])] += int(skip_list[3])
+        #init_list[7+int(skip_list[2])]+=int(skip_list[3]) if int(skip_list[2])>0 else init_list[8+int(skip_list[2])]+=int(skip_list[3])
+        reducer_list.append(init_list)
+        
+    elif overwrite == True:
+        reducer_list[index][2] += int(skip_list[3])
+        if int(skip_list[2])>0:
+            reducer_list[index][7+int(skip_list[2])]+=int(skip_list[3]) 
+        else:
+            reducer_list[index][8+int(skip_list[2])]+=int(skip_list[3]) 
+        #reducer_list[index][7+int(skip_list[2])]+=int(skip_list[3]) if int(skip_list[2])>0 else reducer_list[index][8+int(skip_list[2])]+=int(skip_list[3])
+    return reducer_list
 
 if __name__== "__main__" :
-
+    signal(SIGPIPE, SIG_IGN)
+    pviot = ''; word ='';reducer_dict = {}; reducer_list = []; index = 0; count=0
     for line in sys.stdin:
-        line = line.strip()
-        if not line: continue
-
         # [ TODO ] collect the output from shuffler and generate reducer output
         # Now you need to calculate the skip-gram frequency with its distance information.
         # Input: 
@@ -19,4 +44,23 @@ if __name__== "__main__" :
         #   2) Check if this is the same skipgram
         #   3) If so, add the frequency according to its distance
         #   4) If not, output the previous skipgram data
-        ...
+        skip_list = line.strip().split()
+        if pviot != skip_list[0]: #pivot not same
+            pviot = skip_list[0]; word = skip_list[1]
+            reducer_list = generate_reducer(reducer_list, skip_list)
+            index += 1
+
+        elif (pviot == skip_list[0]) and (word != skip_list[1]):
+            word = skip_list[1]
+            reducer_list = generate_reducer(reducer_list, skip_list)
+            index += 1          
+        elif (pviot == skip_list[0]) and (word == skip_list[1]):
+            reducer_list = generate_reducer(reducer_list, skip_list, index-1, True)
+        
+    for reducer in reducer_list:
+        print('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'%(
+            reducer[0], reducer[1], reducer[2], reducer[3], 
+            reducer[4], reducer[5], reducer[6], reducer[7],
+            reducer[8], reducer[9], reducer[10], reducer[11], reducer[12]))
+        if not line: continue
+        
